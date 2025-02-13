0db94e20e870766cd05a06fb2487574a12ae4445
Youssef <yousseflouhichi24@gmail.com>
2025-02-13 20:10:58 +0100

    start designing the navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"main",title:"EasySql",component:MainComponent,children:[
+    
+  ]},
+  {path:"",redirectTo:"main",pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
