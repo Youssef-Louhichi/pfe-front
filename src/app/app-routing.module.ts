12f81e9863c26716d8722120b39f3b84358d9244
joozzz <youssefbenfarah232@gmail.com>
2025-02-15 19:25:23 +0100

    some changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
 import { ConnectionsComponent } from './components/connections/connections.component';
+import { UsersComponent } from './components/users/users.component';
 
 const routes: Routes = [
   {path:"main",title:"EasySql",component:MainComponent,children:[
     
-    {path:"connections",component:ConnectionsComponent}
+    {path:"connections",component:ConnectionsComponent},
+    {path:"users",component:UsersComponent}
   ]},
   {path:"",redirectTo:"main",pathMatch:'full'}
 ];
