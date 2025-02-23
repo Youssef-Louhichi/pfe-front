b3b2ca1953171883581213426b0662a597222715
Youssef <yousseflouhichi24@gmail.com>
2025-02-23 16:26:28 +0100

    home page design integrated

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Connexion } from 'src/app/models/connexion';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  constructor(private userService:UsersService){}
+
+  connexions:Connexion[]
+
+  ngOnInit(): void {
+    this.userService.getUsersConnexions(1).subscribe(data =>{
+      //console.log(data)
+      this.connexions = data
+    })
+  }
+
+
+
 
 }
