9ec7790f41ece5263fe8e3241c99ed5eb883e5c0
Youssef <yousseflouhichi24@gmail.com>
2025-02-23 16:04:28 +0100

    connexion updated

--- src/app/models/connexion.ts
+++ src/app/models/connexion.ts
@@ -1,7 +1,7 @@
 export class Connexion {
 
     constructor(public id:number,public host:String,public port:String,public username:String,
-        public pwd:String,public dbtype:String,public databaseName:String,public createdAt:Date,
+        public pwd:String,public dbtype:String,public createdAt:Date,
         public updatedAt:Date){}
 
 }
